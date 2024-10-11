d35ba63c5f4a6f963a9ea06eff26fadfe2ae591b
amandamigliorini <amanda.migliorini@hotmail.com>
2024-10-11 18:59:59 -0300

    changing message

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req,res, next) => {
     if(req.session.user === undefined){
-        return res.status(401).json("You do not have success");
+        return res.status(401).json("You do not have authorization to execute this request");
     }
     next();
 };
